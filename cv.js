0e2d58f2aa258cecf23880b3782c10da944ed19f
ngoccuongpvc <ngoccuongpvc@gmail.com>
2019-12-17 23:00:47 +0700

    Create cv.js

---
+++ cv.js
@@ -0,0 +1,6 @@
+$(".title").on("mouseover",function(){
+	$(this).css({"background":"#273c9c","color":"white"});
+});
+$(".title").on("mouseout",function(){
+	$(this).css({"background":"#f7f5f5","color":"#273c9c"});
+});
