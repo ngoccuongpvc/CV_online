4e05797a830cc270e2e0ac6d66cf6f2840e0766f
ngoccuongpvc <ngoccuongpvc@gmail.com>
2019-12-18 09:05:00 +0700

    Update cv.js

--- cv.js
+++ cv.js
@@ -4,3 +4,26 @@ $(".title").on("mouseover",function(){
 $(".title").on("mouseout",function(){
 	$(this).css({"background":"#f7f5f5","color":"#273c9c"});
 });
+$(".name").on("mouseover",function(){
+	$(this).css({"background":"#e8e6e6", "box-shadow":"1px 1px 30px lightgray"});
+});
+
+$(".name").on("mouseout",function(){
+	$(this).css({"background":"#f7f5f5", "box-shadow":"none"});
+});
+
+$(".email").on("mouseover",function(){
+	$(this).css({"background":"#e8e6e6", "box-shadow":"1px 1px 30px lightgray"});
+});
+
+$(".email").on("mouseout",function(){
+	$(this).css({"background":"#f7f5f5", "box-shadow":"none"});
+});
+
+$("h3,h4,h5").on("mouseenter",function(){
+	$(this).css({"font-size":Number($(this).css("font-size").slice(0,2))+2 + "px", "box-shadow":"1px 1px 500px lightgray"});
+});
+
+$("h3,h4,h5").on("mouseout",function(){
+	$(this).css({"font-size":Number($(this).css("font-size").slice(0,2))-2 + "px","box-shadow":"none"});
+});
